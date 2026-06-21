7e24cbefe4d43cc9cbdfbfdb5fe59bc412a7522a
Raul-home <raul.guedes@trashin.com.br>
2026-06-21 13:48:24 -0300

    Commit inicial

---
+++ resources/views/carta/update.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Carta update')
+
+@section('content')
+    <h1>Carta - {{$id}}</h1>
+@endsection
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="row">
+    <div class="col-md-12">
+    <h2>Menu</h2>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Coleção</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Deck</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Loja</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Booster</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Partida</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Perfil</a>
+    </div>
+
+    ADMINISTRAÇÃO
+    --------------------------
+    <div class="col-md-12">
+        <a href="/carta/update">Carta</a>
+    </div>
+
+    <div class="col-md-12">
+        <a href="/">Booster</a>
+    </div>
+</div>
